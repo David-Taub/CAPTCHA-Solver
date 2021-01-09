e90697dac55a6433e8daec94919cbc3cd4617ef1
David Taub <davidtaub333@gmail.com>
2021-01-09 23:28:40 +0200

    use pil paste

--- main.py
+++ main.py
@@ -10,27 +10,29 @@ def create_image_char(image_size, background, character, char_color, char_pos, c
     return image
 
 
-def generate_char_image(character, image_size=(100, 100), background_color=(255, 255, 255, 255), text_color=(0, 0, 0, 255),
-                        font_path=r'fonts\FreeMono.ttf', font_size=100, start_margins=(0, 0)):
+def generate_char_image(character, image_size=(100, 100), background_color=(255, 255, 255),
+                        text_color=(0, 0, 0), font_path=r'fonts\FreeMono.ttf', font_size=100,
+                        start_margins=(0, 0)):
     font = ImageFont.truetype(font_path, font_size)
     image = create_image_char(image_size, background_color, character,
                               text_color, start_margins, font)
-    # W x H x C
-    return np.array(image)
+    # H x W x C
+    return image
 
 
-def generate_multi_char_image(characters, image_size=[100, 400], background_color=(255, 255, 255, 255)):
+def generate_multi_char_image(characters, image_size=[100, 600], background_color=(255, 255, 255)):
     imgs = [generate_char_image(character=c) for c in characters]
-    background_img = np.tile([[background_color]], (image_size + [1]))
-    location = 0
+    arr = np.tile([[background_color]], (image_size + [1]))
+    background_img = Image.fromarray(arr.astype(np.uint8))
+    location = 20
     for img in imgs:
-        background_img[:, location:location + img.shape[1], :] = img
-        location += img.shape[1]
+        background_img.paste(img, [location, 0])
+        location += img.size[0]
     return background_img
 
 
 def main():
-    image = generate_multi_char_image('ABC')
+    image = generate_multi_char_image('ABCDEF')
     # image = generate_char_image('A')
     plt.imshow(image)
     plt.show()
