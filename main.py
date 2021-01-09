3449b13160bd788793eb6f6bf11e3c871e07ebde
David Taub <davidtaub333@gmail.com>
2021-01-09 23:06:11 +0200

    multichar image generation

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image, ImageFont, ImageDraw
 
@@ -9,16 +10,28 @@ def create_image_char(image_size, background, character, char_color, char_pos, c
     return image
 
 
-def generate_text_image(text='ABCDab', image_size=(400, 100), background_color=(255, 255, 255), text_color=(0, 0, 0),
-                        font_path=r'fonts\FreeMono.ttf', font_size=100, start_margins=(15, 0)):
+def generate_char_image(character, image_size=(100, 100), background_color=(255, 255, 255, 255), text_color=(0, 0, 0, 255),
+                        font_path=r'fonts\FreeMono.ttf', font_size=100, start_margins=(0, 0)):
     font = ImageFont.truetype(font_path, font_size)
-    image = create_image_char(image_size, background_color, text,
+    image = create_image_char(image_size, background_color, character,
                               text_color, start_margins, font)
-    return image
+    # W x H x C
+    return np.array(image)
+
+
+def generate_multi_char_image(characters, image_size=[100, 400], background_color=(255, 255, 255, 255)):
+    imgs = [generate_char_image(character=c) for c in characters]
+    background_img = np.tile([[background_color]], (image_size + [1]))
+    location = 0
+    for img in imgs:
+        background_img[:, location:location + img.shape[1], :] = img
+        location += img.shape[1]
+    return background_img
 
 
 def main():
-    image = generate_text_image()
+    image = generate_multi_char_image('ABC')
+    # image = generate_char_image('A')
     plt.imshow(image)
     plt.show()
 
