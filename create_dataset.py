4afd6f672263a49b4b4574de4c26c5dd5e634b55
David Taub <davidtaub333@gmail.com>
2021-01-17 01:09:24 +0200

    chars offset added mult level dataset now created capcha generator is now a class

--- create_dataset.py
+++ create_dataset.py
@@ -1,44 +1,36 @@
+import sys
 import json
 import os
-import generate_captcha
 import csv
 import tqdm
+import shutil
+
+import generate_captcha
 
 
-def create_dataset(dir_path, size):
-    ID_LENGTH = 8
-    TEXT_LENGTH = 6
-    IMAGE_WIDTH = 600
-    IMAGE_HEIGHT = 100
+def create_dataset(dir_path, size, generator):
+    id_length = len(str(size))
 
-    if os.path.isdir(dir_path) and len(os.listdir(dir_path)) == size + 1:
-        return
+    shutil.rmtree(dir_path, ignore_errors=True)
     os.makedirs(dir_path)
-    alphabet = generate_captcha.get_alphabet()
     with open(os.path.join(dir_path, 'metadata.json'), 'w', encoding='utf-8') as f:
-        json.dump({'alphabet': alphabet,
-                   'image_width': IMAGE_WIDTH,
-                   'image_height': IMAGE_HEIGHT,
-                   'text_length': TEXT_LENGTH}, f)
+        json.dump({'alphabet': generator.alphabet,
+                   'image_width': generator.image_size[1],
+                   'image_height': generator.image_size[0],
+                   'text_length': generator.length}, f)
     with open(os.path.join(dir_path, 'strings.csv'), 'w', newline='', encoding='utf-8') as f:
         writer = csv.writer(f, delimiter=',')
-        for i in tqdm.tqdm(range(size)):
-            sample_id = str(i).zfill(ID_LENGTH)
-            img, text = generate_captcha.generate_captcha(alphabet, [IMAGE_HEIGHT, IMAGE_WIDTH], TEXT_LENGTH)
+        for i in tqdm.tqdm(range(size), desc=dir_path):
+            sample_id = str(i).zfill(id_length)
+            img, text = generator.generate_captcha()
             img.save(os.path.join(dir_path, f'{sample_id}.jpg'))
             writer.writerow([sample_id, text])
 
 
 def main():
-    create_dataset(r'data\train', 1000)
-    create_dataset(r'data\test', 1000)
-    # import CaptchaDataset
-    # d = CaptchaDataset.CaptchaDataset(r'data\train')
-    # print(len(d))
-    # print(d[4])
-    # import matplotlib.pyplot as plt
-    # plt.imshow(d[4][0])
-    # plt.show()
+    for i, generator in enumerate(generate_captcha.generators):
+        create_dataset(rf'data\level_{i}\train', int(sys.argv[1]), generator)
+        create_dataset(rf'data\\level_{i}\test', 100, generator)
 
 
 if __name__ == '__main__':
--- generate_captcha.py
+++ generate_captcha.py
@@ -14,55 +14,82 @@ random.seed(42)
 # TODO: add lines and shapes
 
 
-def create_image_char(char_image_size, background, character, char_color, char_pos, char_font):
-    image = Image.new("RGBA", char_image_size, background)
-    draw = ImageDraw.Draw(image)
-    draw.text(char_pos, character, fill=char_color, font=char_font)
-    return image
+class CaptchGenerator:
+    def __init__(self,
+                 alphabet=string.ascii_lowercase, image_size=(100, 600), length=6, offset_range=(0, 1),
+                 start_offset=20, char_image_size=(100, 100), background_color=(255, 255, 255, 255),
+                 char_color=(0, 0, 0, 255), font_path=r'.\fonts\FreeMono.ttf', font_size=100, start_margins=(0, 0)):
+        self.alphabet = alphabet
+        self.image_size = image_size
+        self.length = length
+        self.offset_range = offset_range
+        self.start_offset = start_offset
+        self.char_image_size = char_image_size
+        self.background_color = background_color
+        self.char_color = char_color
+        self.font_path = font_path
+        self.font_size = font_size
+        self.start_margins = start_margins
+
+    def _generate_char_image(self, character):
+        font_obj = ImageFont.truetype(self.font_path, self.font_size)
+        image = Image.new("RGBA", self.char_image_size, (255, 255, 255, 0))
+        draw = ImageDraw.Draw(image)
+        draw.text((0, 0), character, fill=self.char_color, font=font_obj)
+        # H x W x C
+        return image
+
+    def _generate_multi_char_image(self, characters):
+        imgs = [self._generate_char_image(character=c) for c in characters]
+        blank_background = np.tile([[self.background_color]], (list(self.image_size) + [1]))
+        background_img = Image.fromarray(blank_background.astype(np.uint8))
+        location = self.start_offset
+        for img in imgs:
+            offset = random.randrange(*self.offset_range)
+            background_img.alpha_composite(img, (location, 0))
+            location += img.size[0] + offset
+        return background_img.convert('RGB')
+
+    def get_alphabet(self, with_capitals=True, with_numerics=True):
+        alphabet = string.ascii_lowercase
+        if with_capitals:
+            alphabet += string.ascii_uppercase
+        if with_numerics:
+            alphabet += string.digits
+        return alphabet
+
+    def _generate_random_characters(self):
+        return ''.join(random.choice(self.alphabet) for x in range(self.length))
+
+    def generate_captcha(self):
+        characters = self._generate_random_characters()
+        image = self._generate_multi_char_image(characters)
+        # image = np.array(image)
+        return image, characters
 
 
-def generate_char_image(character, char_image_size=(100, 100), background_color=(255, 255, 255),
-                        text_color=(0, 0, 0), font_path=r'fonts\FreeMono.ttf', font_size=100,
-                        start_margins=(0, 0)):
-    font = ImageFont.truetype(font_path, font_size)
-    image = create_image_char(char_image_size, background_color, character,
-                              text_color, start_margins, font)
-    # H x W x C
-    return image
-
-
-def generate_multi_char_image(characters, image_size, background_color=(255, 255, 255)):
-    imgs = [generate_char_image(character=c) for c in characters]
-    arr = np.tile([[background_color]], (image_size + [1]))
-    background_img = Image.fromarray(arr.astype(np.uint8))
-    location = 20
-    for img in imgs:
-        background_img.paste(img, [location, 0])
-        location += img.size[0]
-    return background_img
-
-
-def get_alphabet(with_capitals=True, with_numerics=True):
-    alphabet = string.ascii_lowercase
-    if with_capitals:
-        alphabet += string.ascii_uppercase
-    if with_numerics:
-        alphabet += string.digits
-    return alphabet
-
-
-def rand_text_generator(alphabet, length):
-    return ''.join(random.choice(alphabet) for x in range(length))
-
-
-def generate_captcha(alphabet, image_size=[100, 600], length=6):
-    text = rand_text_generator(alphabet, length)
-    image = generate_multi_char_image(text, image_size)
-    return image, text
+# TODO: move to dataset creator
+generators = [
+    CaptchGenerator(alphabet=string.ascii_lowercase, image_size=(100, 600), length=6, offset_range=(0, 1),
+                    start_offset=20, char_image_size=(100, 100), background_color=(255, 255, 255, 255),
+                    char_color=(0, 0, 0, 255), font_path=r'.\fonts\FreeMono.ttf', font_size=100, start_margins=(0, 0)),
+    CaptchGenerator(alphabet=string.ascii_lowercase + string.ascii_uppercase + string.digits,
+                    image_size=(100, 600), length=6, offset_range=(0, 1),
+                    start_offset=20, char_image_size=(100, 100), background_color=(255, 255, 255, 255),
+                    char_color=(0, 0, 0, 255), font_path=r'.\fonts\FreeMono.ttf', font_size=100, start_margins=(0, 0)),
+    CaptchGenerator(alphabet=string.ascii_lowercase + string.ascii_uppercase + string.digits,
+                    image_size=(100, 600), length=6, offset_range=(-40, 0),
+                    start_offset=20, char_image_size=(100, 100), background_color=(255, 255, 255, 255),
+                    char_color=(0, 0, 0, 255), font_path=r'.\fonts\FreeMono.ttf', font_size=100, start_margins=(0, 0)),
+    CaptchGenerator(alphabet=string.ascii_lowercase + string.ascii_uppercase + string.digits,
+                    image_size=(100, 600), length=6, offset_range=(-80, -30),
+                    start_offset=20, char_image_size=(100, 100), background_color=(255, 255, 255, 255),
+                    char_color=(0, 0, 0, 255), font_path=r'.\fonts\FreeMono.ttf', font_size=100, start_margins=(0, 0)),
+]
 
 
 def main():
-    plt.imshow(generate_captcha())
+    plt.imshow(generators[-1].generate_captcha()[0])
     plt.show()
 
 
