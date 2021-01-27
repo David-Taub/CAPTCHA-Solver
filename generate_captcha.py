c7d0b82be4fd6e44fb70cad4f879376f048143c2
David Taub <davidtaub333@gmail.com>
2021-01-27 18:51:47 +0200

    char size fixed

--- generate_captcha.py
+++ generate_captcha.py
@@ -19,27 +19,30 @@ class CaptchGenerator:
         np.random.seed(seed)
 
     def __init__(self,
-                 alphabet=string.ascii_lowercase, image_size=(100, 600), length=6, offset_range=((0, 1), (0, 1)),
-                 rotation_range=(-60, 60),
-                 start_offset=20, char_image_size=(100, 100), background_color=(255, 255, 255, 255),
-                 char_color=(0, 0, 0, 255), font_path=r'.\fonts\FreeMono.ttf', font_size=100, start_margins=(0, 0)):
+                 alphabet=string.ascii_lowercase, image_size=(100, 600), length_range=(6, 7),
+                 offset_range=((0, 1), (0, 1)), rotation_range=(-60, 60),
+                 start_offset=20, background_color=(255, 255, 255, 255),
+                 char_color=(0, 0, 0, 255), font_path=r'.\fonts\FreeMono.ttf', font_size_range=(100, 101),
+                 start_margins=(0, 0)):
         self.alphabet = alphabet
         self.image_size = image_size
-        self.length = length
+        self.length_range = length_range
         self.offset_range = offset_range
         self.start_offset = start_offset
         self.rotation_range = rotation_range
-        self.char_image_size = char_image_size
         self.background_color = background_color
         self.char_color = char_color
         self.font_path = font_path
-        self.font_size = font_size
+        self.font_size_range = font_size_range
         self.start_margins = start_margins
         self.reset_seed()
 
     def _generate_char_image(self, character):
-        font_obj = ImageFont.truetype(self.font_path, self.font_size)
-        image = Image.new("RGBA", self.char_image_size, (255, 255, 255, 0))
+        font_size = random.randrange(*self.font_size_range)
+        font_obj = ImageFont.truetype(self.font_path, font_size)
+        image_height = sum(font_obj.getmetrics())
+        image_width = font_obj.font.getsize(character)[0][0]
+        image = Image.new("RGBA", (image_width, image_height), (255, 255, 255, 0))
         draw = ImageDraw.Draw(image)
         draw.text((0, 0), character, fill=self.char_color, font=font_obj)
         # H x W x C
@@ -53,8 +56,8 @@ class CaptchGenerator:
         for img in imgs:
             rotation_angle = random.randrange(*self.rotation_range)
             img_width = img.size[0]
-            img = img.rotate(rotation_angle, expand=True)
-            base_y_offset = (background_img.size[1] - img.size[1]) * 0.5
+            img = img.rotate(rotation_angle, resample=Image.BICUBIC, expand=True)
+            base_y_offset = (background_img.size[1] - img.size[1]) / 2
             x_offset = random.randrange(*self.offset_range[0]) + img_width
             y_offset = int(base_y_offset + random.randrange(*self.offset_range[1]))
             # import pdb
@@ -75,44 +78,50 @@ class CaptchGenerator:
         return alphabet
 
     def _generate_random_characters(self):
-        return ''.join(random.choice(self.alphabet) for x in range(self.length))
+        length = random.randrange(*self.length_range)
+        return ''.join(random.choice(self.alphabet) for x in range(length))
 
     def generate_captcha(self):
         characters = self._generate_random_characters()
         image = self._generate_multi_char_image(characters)
-        # image = np.array(image)
         return image, characters
 
 
 # TODO: move to dataset creator
 generators = [
-    CaptchGenerator(alphabet=string.ascii_lowercase, image_size=(100, 600), length=6, offset_range=((0, 1), (0, 1)),
-                    rotation_range=(0, 1),
-                    start_offset=20, char_image_size=(100, 100), background_color=(255, 255, 255, 255),
-                    char_color=(0, 0, 0, 255), font_path=r'.\fonts\FreeMono.ttf', font_size=100, start_margins=(0, 0)),
-    CaptchGenerator(alphabet=string.ascii_lowercase + string.ascii_uppercase + string.digits,
-                    image_size=(100, 600), length=6, offset_range=((-5, 5), (-5, 5)),
+    CaptchGenerator(alphabet=string.ascii_lowercase,
+                    image_size=(100, 600), length_range=(6, 7), offset_range=((10, 11), (0, 1)), rotation_range=(0, 1),
+                    start_offset=20, background_color=(255, 255, 255, 255),
+                    char_color=(0, 0, 0, 255), font_path=r'.\fonts\FreeMono.ttf', font_size_range=(90, 91),
+                    start_margins=(0, 0)),
+    CaptchGenerator(alphabet=string.ascii_lowercase + string.ascii_uppercase,
+                    image_size=(100, 600), length_range=(6, 8), offset_range=((7, 13), (-5, 5)),
                     rotation_range=(-5, 5),
-                    start_offset=20, char_image_size=(100, 100), background_color=(255, 255, 255, 255),
-                    char_color=(0, 0, 0, 255), font_path=r'.\fonts\FreeMono.ttf', font_size=100, start_margins=(0, 0)),
+                    start_offset=20, background_color=(255, 255, 255, 255),
+                    char_color=(0, 0, 0, 255), font_path=r'.\fonts\FreeMono.ttf', font_size_range=(90, 91),
+                    start_margins=(0, 0)),
     CaptchGenerator(alphabet=string.ascii_lowercase + string.ascii_uppercase + string.digits,
-                    image_size=(100, 600), length=6, offset_range=((-45, -10), (-15, 15)),
+                    image_size=(100, 600), length_range=(5, 10), offset_range=((-5, 10), (-15, 15)),
                     rotation_range=(-30, 30),
-                    start_offset=20, char_image_size=(100, 100), background_color=(255, 255, 255, 255),
-                    char_color=(0, 0, 0, 255), font_path=r'.\fonts\FreeMono.ttf', font_size=100, start_margins=(0, 0)),
+                    start_offset=20, background_color=(255, 255, 255, 255),
+                    char_color=(0, 0, 0, 255), font_path=r'.\fonts\FreeMono.ttf', font_size_range=(90, 91),
+                    start_margins=(0, 0)),
     CaptchGenerator(alphabet=string.ascii_lowercase + string.ascii_uppercase + string.digits,
-                    image_size=(100, 600), length=6, offset_range=((-60, -30), (-25, 25)),
+                    image_size=(100, 600), length_range=(5, 11), offset_range=((-15, 0), (-20, 20)),
                     rotation_range=(-60, 60),
-                    start_offset=20, char_image_size=(100, 100), background_color=(255, 255, 255, 255),
-                    char_color=(0, 0, 0, 255), font_path=r'.\fonts\FreeMono.ttf', font_size=100, start_margins=(0, 0)),
+                    start_offset=20, background_color=(255, 255, 255, 255),
+                    char_color=(0, 0, 0, 255), font_path=r'.\fonts\FreeMono.ttf', font_size_range=(70, 131),
+                    start_margins=(0, 0)),
 ]
 
 
 def main():
     N = 4
-    for i in range(2 * N):
-        plt.subplot(N, 2, i + 1)
-        plt.imshow(generators[-2].generate_captcha()[0])
+    M = 4
+    for i in range(N):
+        for j in range(M):
+            plt.subplot(N, M, i * M + j + 1)
+            plt.imshow(generators[-i - 1].generate_captcha()[0])
     plt.show()
 
 
